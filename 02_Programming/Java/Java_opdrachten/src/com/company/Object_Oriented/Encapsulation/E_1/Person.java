43da37ca36d0cd27c3f672fb512f6a195656e349
MajaMana <marijanam-98@hotmail.com>
2022-06-22 15:35:00 +0200

    chapter 1

--- 02_Programming/Java/Java_opdrachten/src/com/company/Object_Oriented/Encapsulation/E_1/Person.java
+++ 02_Programming/Java/Java_opdrachten/src/com/company/Object_Oriented/Encapsulation/E_1/Person.java
@@ -1,4 +1,13 @@
 package com.company.Object_Oriented.Encapsulation.E_1;
 
 public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
 }
