eac84dc39bdbb8c8dfb34a1601ba724a2638959d
MajaMana <marijanam-98@hotmail.com>
2022-06-23 15:49:02 +0200

    environments

--- 02_Programming/Java/Java_opdrachten/src/com/company/Zoo/Main.java
+++ 02_Programming/Java/Java_opdrachten/src/com/company/Zoo/Main.java
@@ -2,15 +2,18 @@ package com.company.Zoo;
 
 public class Main {
     public static void main(String[] args) {
-        Bird bird1 = new Bird("Light", "Jungle");
-        Bird bird2 = new Bird("Kira", "Jungle");
+        Bird light = new Bird("Light", "Jungle");
+        Bird kira = new Bird("Kira", "Jungle");
 
-        Reptile reptile1 = new Reptile("Basil", "Forest");
-        Reptile reptile2 = new Reptile("Medusa", "Forest");
+        Reptile basil = new Reptile("Basil", "Forest");
+        Reptile medusa = new Reptile("Medusa", "Forest");
 
-        Cat cat1 = new Cat("Forest", "Savannah");
-        Cat cat2 = new Cat("Morticia", "Forest");
-        Cat cat3 = new Cat("Aurora", "Forest");
-        Cat cat4 = new Cat("Salem", "Savannah");
+        Cat forest = new Cat("Forest", "Savannah");
+        Cat morticia = new Cat("Morticia", "Forest");
+        Cat aurora = new Cat("Aurora", "Forest");
+        Cat salem = new Cat("Salem", "Savannah");
+
+        Zoo zoo = new Zoo(new Animal[] {light, kira, basil, medusa, forest, morticia, aurora, salem});
+        zoo.printAnimals();
     }
 }
--- 02_Programming/Java/Java_opdrachten/src/com/company/Zoo/Zoo.java
+++ 02_Programming/Java/Java_opdrachten/src/com/company/Zoo/Zoo.java
@@ -1,4 +1,18 @@
 package com.company.Zoo;
+import java.util.ArrayList;
 
 public class Zoo {
+    ArrayList<Animal> zooAnimals = new ArrayList<>();
+
+    public void printAnimals() {
+        for (Animal zooAnimal : zooAnimals) {
+            System.out.println("Name: " + zooAnimal.name + "\nEnvironment: " + zooAnimal.environment + "\n");
+        }
+    }
+
+    public Zoo(Animal[] animals) {
+        for (Animal animal : animals) {
+            zooAnimals.add(animal);
+        }
+    }
 }
